b5be71d1a48b8a39a2ed521fa24746843a36b3b1
fyved24 <fyved24@163.com>
2020-05-09 19:11:13 +0800

    exclude datasource config

--- src/main/java/cn/ironz/aowu/App.java
+++ src/main/java/cn/ironz/aowu/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
